433c142a9f5bccf91b1fbcb842b023a58136522f
Mohammad Amin Rajabi <mohammadaminrajabii2004@gmail.com>
2024-12-29 19:14:10 +0330

    add loading page

---
+++ app/loading.js
@@ -0,0 +1,16 @@
+import "./loading.css";
+
+export default function Loading() {
+  return (
+    <div class="lds-roller">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
