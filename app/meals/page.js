9140647ba52d0783bb50b9f347ff8871ea50df06
Mohammad Amin Rajabi <mohammadaminrajabii2004@gmail.com>
2024-12-29 16:21:50 +0330

    add meal item and meal grid

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,28 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+
 function mealsPage() {
   return (
-    <div>
-      <h2>meals 🍔</h2>
-    </div>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Dlicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favoritr recipe and cook it yourself. It is easy and fun
+          😻
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">
+          Share Your Favoritr Recipe
+          </Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        
+      </main>
+    </>
   );
 }
 
