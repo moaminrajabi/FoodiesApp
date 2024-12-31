d4b9f7df05ecdda9cbbfdb65f46ca860f2dd8256
Mohammad Amin Rajabi <mohammadaminrajabii2004@gmail.com>
2024-12-31 18:08:16 +0330

    add input for enter image meal

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,17 +1,39 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import classes from "./image-picker.module.css";
+import Image from "next/image";
 
 export default function ImagePicker({ label, name }) {
+  const [pikedImage, setPikedImage] = useState();
   const ImageInput = useRef();
+
   function handlePickClick() {
     ImageInput.current.click();
   }
+
+  function handleImageChange(e) {
+    const file = e.target.files[0];
+
+    if (!file) {
+      return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPikedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  }
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pikedImage && <p>No image picked yet!</p>}
+          {pikedImage && <Image src={pikedImage} alt="image" fill />}
+        </div>
         <input
           ref={ImageInput}
           className={classes.input}
@@ -19,6 +41,7 @@ export default function ImagePicker({ label, name }) {
           id={name}
           accept="image/png , image/jpeg"
           name={name}
+          onChange={handleImageChange}
         />
         <button
           className={classes.button}
